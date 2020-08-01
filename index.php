c9746d2fec6d9d5e764cf922996c80a312c02f6b
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-08-01 15:06:15 +0100

    updates

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
-<?php
-    print_r($_GET);
-?>
-
-<h1>This is index </h1>
+<?php require_once("./views/_header.html"); ?>
+	<div class="jumbotron">
+		<h1 class="display-4">Camagru</h1>
+		<p class="lead">Make your pictures looks nice with our editor.</p>
+		<hr class="my-4">
+		<a class="btn btn-primary" href="#" role="button">Learn more</a>
+	</div>
+<?php require_once("./views/_footer.html"); ?>
