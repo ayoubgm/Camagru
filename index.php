1b1175a4951d105b70b1bdf9cc77e5a5ce5fe681
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-08-02 08:53:14 +0100

    add the index of the app

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php require_once("./views/_header.html"); ?>
-	<div class="jumbotron">
-		<h1 class="display-4">Camagru</h1>
-		<p class="lead">Make your pictures looks nice with our editor.</p>
-		<hr class="my-4">
-		<a class="btn btn-primary" href="#" role="button">Learn more</a>
-	</div>
-<?php require_once("./views/_footer.html"); ?>
---
+++ public/js/_menu.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click', () => {
+		// Toggle navbar
+        nav.classList.toggle('nav-active');
+		
+		// Animate nav links
+		navLinks.forEach((link, index) => {
+			if ( link.style.animation ) { link.style.animation = ""; }
+			else { link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.5}s`; }
+		});
+
+		// Burger animation
+		burger.classList.toggle('toggle');
+	});
+}
+
+navSlide();
