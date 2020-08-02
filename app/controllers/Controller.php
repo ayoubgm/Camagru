3cdef8704f75d7047448601fe1fe071aa4b416fa
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-08-02 13:24:34 +0100

    Updates

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -1,13 +1,15 @@
 <?php
 	class Controller {
 
-		public function     model( $model ) {
+		public function     call_model( $model )
+		{
 			if ( file_exists('../models/' . $model . '.php') ) {
 				echo "Model exists " . $model; 
 			}
 		}
 
-		public function 	view( $view ) {
+		public function 	call_view( $view )
+		{
 			if ( file_exists(VIEWS . $view . '.php') ) {
 				require_once(VIEWS . $view . '.php');
 			}
--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,23 +1,25 @@
 <?php
-
 	class Home extends Controller{
 
-		public function		index () {
-			$this->view('home' . DIRECTORY_SEPARATOR .'index');
+		public function		index ()
+		{
+			$this->call_view('home' . DIRECTORY_SEPARATOR .'index');
 		}
 
-		public function		signin() {
-			$this->view('home' . DIRECTORY_SEPARATOR .'signin');
+		public function		signin()
+		{
+			$this->call_view('home' . DIRECTORY_SEPARATOR .'signin');
 		}
 
-		public function		signup() {
-			$this->view('home' . DIRECTORY_SEPARATOR .'signup');
+		public function		signup()
+		{
+			$this->call_view('home' . DIRECTORY_SEPARATOR .'signup');
 		}
 
-		public function		notfound() {
+		public function		notfound()
+		{
 			echo "404";
 		}
 
 	}
-
 ?>
