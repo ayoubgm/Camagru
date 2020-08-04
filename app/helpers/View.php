dda00918148bfd47ae7de83fe549f2b1837c2323
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-08-04 22:08:14 +0100

    add a help by view Class

---
+++ app/helpers/View.php
@@ -0,0 +1,21 @@
+<?php
+    class View {
+
+        protected $view_name;
+        protected $view_data;
+
+        public function         __construct( $name, $data = [] )
+        {
+            $this->view_name = $name;
+            $this->view_data = $data;
+        }
+
+        public function         render ()
+        {
+            if ( file_exists(VIEWS . $this->view_name . '.php') ) {
+                require_once(VIEWS . $this->view_name . '.php');
+            }
+        }
+        
+    }
+?>
