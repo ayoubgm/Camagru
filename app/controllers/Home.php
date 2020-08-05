1b9440abf2e020e4aad2492494dcad1a336ce969
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-08-05 20:49:02 +0100

    Complete registration of a user

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,32 +3,50 @@
 
 		public function		index ()
 		{
-			$indexView = $this->call_view('home' . DIRECTORY_SEPARATOR .'index');
-			$indexView->render();
+			$this->call_view('home' . DIRECTORY_SEPARATOR .'index')->render();
 		}
 
-		public function		signin()
-		{
-			$signinView = $this->call_view('home' . DIRECTORY_SEPARATOR .'signin');
-			$signinView->render();
-		}
-
+		// private function 	sendMail ( $subject, $to ) {
+		// 	switch ( $subject ) {
+		// 		case "Confirmation mail":
+		// 			$headers = 'From: ' .$to . "\r\n".'Reply-To: ' . $to. "\r\n".'X-Mailer: PHP/' . phpversion();
+		// 			if ( mail($to, $subject, "HELLO", $headers) )
+		// 				echo "Send successfull";
+		// 			else
+		// 				echo "not send";
+		// 		break;
+		// 	}
+		// }
+		
 		public function		signup()
 		{
 			switch($_SERVER['REQUEST_METHOD']) {
 				case 'GET':
-					$signupView = $this->call_view('home' . DIRECTORY_SEPARATOR .'signup');
-					$signupView->render();
-					break;
+					$this->call_view('home' . DIRECTORY_SEPARATOR .'signup')->render();
+				break;
 				case 'POST':
 					$userMiddleware = $this->call_middleware('UserMiddleware');
 					$userMiddleware->signup($this, $_POST);
 					$userModel = $this->call_model('UserModel');
-					$userModel->save($_POST);
-					break;
+					try {
+						if ( $userModel->save($_POST) ) {
+							// $this->sendMail("Confirmation mail", strtolower($_POST['email']));
+							$view = $this->call_view( 'home' . DIRECTORY_SEPARATOR .'signup', [ 'success' => "true", 'msg' => "Successful registration, you will receive an email for activation account !" ])->render();
+						} else {
+							$view = $this->call_view( 'home' . DIRECTORY_SEPARATOR .'signup', [ 'success' => "false", 'msg' => "Registration failed !" ] )->render();
+						}
+					} catch ( Exception $e ) {
+						$view = $this->call_view( 'home' . DIRECTORY_SEPARATOR .'signup', [ 'success' => "false", 'msg' => "Something goes wrong, try later !" ] )->render();
+					}
+				break;
 			}
 		}
-
+		
+		public function		signin()
+		{
+			$this->call_view('home' . DIRECTORY_SEPARATOR .'signin')->render();
+		}
+		
 		public function		notfound()
 		{
 			echo "404";
--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -2,17 +2,18 @@
 	class UserModel extends DB {
 
 		public function 	save ( $data ) {
-			$registerData = [
-				':firstname' => strtolower($data['firstname']),
-				':lastname' => strtolower($data['lastname']),
-				':username' => strtolower($data['username']),
-				':email' => strtolower($data['email']),
-				':address' => strtolower($data['address']),
-				':password' => password_hash($data['password'], PASSWORD_ARGON2I),
-				':activationToken' => base64_encode( strtolower($data['email']) . date(now) )
-			];
+			$query = 'INSERT INTO users (firstname, lastname, username, email, `address`, `password`, activationToken) VALUES (?,?,?,?,?,?,?)';
+			$stt = $this->connect()->prepare( $query );
+			return $stt->execute(array(
+				strtolower($data['firstname']),
+				strtolower($data['lastname']),
+				strtolower($data['username']),
+				strtolower($data['email']),
+				strtolower($data['address']),
+				password_hash($data['password'], PASSWORD_ARGON2I),
+				base64_encode( strtolower($data['email']) . date("Y-m-d H:i:s") )
+			));
 
-			$stt = $this->connect()->prepare("INSERT INTO `users` ()");
 		}
 
 	}
