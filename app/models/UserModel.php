1a23ca48d066e5e13efac4656e83ea9854c54739
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-08-07 17:19:42 +0100

    add request to for edit infos

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -1,24 +1,48 @@
 <?php
 	class UserModel extends DB {
 
+		public function 	findUserByUsername ( $username ) {
+			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE username = ?");
+			$stt->execute([ $username ]);
+			return( $stt->fetch(PDO::FETCH_ASSOC) );
+		}
+
+		public function 	findUserById ( $userid ) {
+			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE id = ?");
+			$stt->execute([ $userid ]);
+			$data =  $stt->fetch(PDO::FETCH_ASSOC);
+			return array(
+				'id' => $data['id'],
+				'firstname' => $data['firstname'],
+				'lastname' => $data['lastname'],
+				'username' => $data['username'],
+				'email' => $data['email'],
+				'gender' => $data['gender'],
+				'address' => $data['address'],
+				'createdat' => $data['createdat']
+			);
+		}
+
 		public function 	save ( $data ) {
-			$query = 'INSERT INTO users (firstname, lastname, username, email, `address`, `password`, activationToken) VALUES (?,?,?,?,?,?,?)';
+			$query = 'INSERT INTO users (firstname, lastname, username, email, gender, `address`, `password`, activationToken) VALUES (?,?,?,?,?,?,?,?)';
 			$stt = $this->connect()->prepare( $query );
 			return $stt->execute(array(
 				strtolower($data['firstname']),
 				strtolower($data['lastname']),
 				strtolower($data['username']),
 				strtolower($data['email']),
+				strtolower($data['gender']),
 				strtolower($data['address']),
 				password_hash($data['password'], PASSWORD_ARGON2I),
 				base64_encode( strtolower($data['email']) . date("Y-m-d H:i:s") )
 			));
 		}
 
-		public function 	findUserByUsername ( $username ) {
-			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE username = ?");
-			$stt->execute([ $username ]);
-			return( $stt->fetch() );
+		public function		edit ( $userID, $editedData ) {
+			$editedData['id'] = $userID;
+			$query = 'UPDATE `users` SET firstname = ?, lastname = ?, username = ?, email = ?, gender = ?, `address` = ? WHERE id = ?';
+			$stt = $this->connect()->prepare( $query );
+			return $stt->execute( array_values($editedData) );
 		}
 
 	}
