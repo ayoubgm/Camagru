6e5b938d35e4cc995c54a21c10e8e8cedf0b78a8
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-08-08 17:27:46 +0100

    Fix and update router app

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 	define("ROOT", __DIR__ . DIRECTORY_SEPARATOR);
 	define("APP", ROOT . 'app' . DIRECTORY_SEPARATOR);
-	define("HELPERS", APP . 'helpers' . DIRECTORY_SEPARATOR);
 	define("CONTROLLERS", APP . 'controllers' . DIRECTORY_SEPARATOR);
+	define("HELPERS", APP . 'helpers' . DIRECTORY_SEPARATOR);
 	define("MIDDLEWARES", APP . 'middlewares' . DIRECTORY_SEPARATOR);
 	define("MODELS", APP . 'models' . DIRECTORY_SEPARATOR);
 	define("VIEWS", APP . 'views' . DIRECTORY_SEPARATOR);
-	define("VALIDATORS", APP . 'views' . DIRECTORY_SEPARATOR);
 	define("CONFIG", ROOT . 'config' . DIRECTORY_SEPARATOR);
 	define("PUBLIC_DIR", ROOT . 'public' . DIRECTORY_SEPARATOR);
 	define("CSS", PUBLIC_DIR . 'css' . DIRECTORY_SEPARATOR);
