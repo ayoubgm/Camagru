9edcc5885bcbeeac509cd1863643ae71d54fdaa3
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-08-13 20:51:51 +0100

    update router app

--- app/helpers/router.php
+++ app/helpers/router.php
@@ -13,34 +13,37 @@
 			if ( isset($url[0]) && file_exists( CONTROLLERS . strtolower($url[0]) .'Controller.php' ) ) {
 				$this->controller = strtolower( $url[0] ) . 'Controller';
 				unset($url[0]);
-				$this->controller = new $this->controller();
-				if ( isset($url[1]) && method_exists($this->controller, strtolower($url[1])) ) {
-					$this->method = strtolower($url[1]); unset($url[1]);
-				} else if ( !method_exists($this->controller, $this->method) ) {
-					$this->controller = new homeController();
-					$this->method = 'notfound';
+				if ( isset($url[1]) ) {
+					$this->controller = new $this->controller();
+					if ( method_exists($this->controller, strtolower($url[1])) ) {
+						$this->method = strtolower($url[1]);
+						unset($url[1]);
+					} else {
+						$this->method = 'notfound';
+					}
 				}
 			} else {
 				$this->controller = new $this->controller();
 				// if $url[0] not a controller it can be a method of home controller 
 				if ( isset($url[0]) ) {
-					if ( method_exists($this->controller, strtolower($url[0]) ) ){ $this->method = strtolower($url[0]); unset($url[0]); }
-					else { $this->method = 'notfound'; }
+					if ( method_exists($this->controller, strtolower($url[0]) ) ) {
+						$this->method = strtolower($url[0]);
+						unset($url[0]);
+					} else { $this->method = 'notfound'; }
 				}
 			}
+			// Create or recreate database `db_camagru` on the mysql server 
 			// if ( $this->controller instanceof homeController && $this->method === "index" ) {
 			// 	$objsetup = new Setup();
 			// 	$objsetup->setup();
 			// }
-			$this->params = $url ? array_values($url) : [];
+			$this->params = $url ? array_values( $url ) : [];
 			call_user_func_array([$this->controller, $this->method], [ $this->params ]);
 		}
 		
 		protected function parseURL()
 		{
-			if ( isset($_GET['url']) ) {
-				return  $url = explode('/', filter_var(rtrim(strtolower($_GET['url']), '/'), FILTER_SANITIZE_URL));
-			}
+			if ( isset($_GET['url']) ) { return  $url = explode('/', filter_var(rtrim(strtolower($_GET['url']), '/'), FILTER_SANITIZE_URL)); }
 		}
 	}
 ?>
