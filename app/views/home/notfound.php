b5e15dab195db1c6b3de16b1618cb6974b1bfc00
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-08-13 20:51:06 +0100

    add not found view with css

---
+++ app/views/home/notfound.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php echo PUBLIC_FOLDER; ?>/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="<?php echo PUBLIC_FOLDER; ?>/css/notfound.css"/>
+	<link rel="stylesheet" href="<?php echo PUBLIC_FOLDER; ?>/css/_header.css"/>
+	<link rel="stylesheet" href="<?php echo PUBLIC_FOLDER; ?>/css/_footer.css"/>
+    <title>Not found</title>
+</head>
+<body>
+    <div class="container offset-1 col-lg-10">
+        <img src="https://media.giphy.com/media/d2Z4NRCUxsxZBvag/giphy.gif"/>
+    </div>
+</body>
+</html>
