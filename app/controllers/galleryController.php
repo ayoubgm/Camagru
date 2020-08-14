3340b6e63afcef698935eb9b211f10e713d7b52f
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-08-14 21:18:46 +0100

    update gallery controller

--- app/controllers/galleryController.php
+++ app/controllers/galleryController.php
@@ -2,42 +2,48 @@
 
 	class galleryController extends Controller {
 
+		private $userModel;
+		private $galleryModel;
+
 		public function         __construct()
 		{
 			session_start();
+			$this->userModel = self::call_model('UserModel');
+			$this->galleryModel = self::call_model('GalleryModel');
 		}
 		
-		// public function			index ( $data )
-		// {
-		// 	/* Load all images edited by users  */
-		// 	$userid = ( isset( $_SESSION['userid'] ) ) ? $_SESSION['userid'] : null;
-		// 	$userData = ( $userid == null ) ? [] : $this->call_model('UserModel')->findUserById( $userid );
-		// 	$page = 1;
-		// 	$imagePerPage = 5;
-		// 	if ( isset( $data[0] ) && $data[0] === "page" && !empty( $data[1] ) && $data[1] > 0 ) {
-		// 		$page = intval($data[1]);
-		// 	}
-		// 	$depart = ( $page - 1 ) * $imagePerPage;
-		// 	$gallery = $this->call_model('GalleryModel')->getAllEditedImages( $depart, $imagePerPage );
-		// 	$totalImages = $this->call_model('GalleryModel')->getCountImages();
+		public function			index ( $data )
+		{
+			/* Load all images edited by users  */
+			if ( isset( $_SESSION['userid'] ) && !empty( $_SESSION['userid'] ) ) {
+				$userData = $this->userModel->findUserById( $_SESSION['userid'] );
+				$userGallery = $this->userModel->findUserById( $_SESSION['userid'] );
+			}
+			$page = 1;
+			$imagePerPage = 5;
+			if ( isset( $data[0] ) && $data[0] === "page" && !empty( $data[1] ) && $data[1] > 0 ) { $page = intval($data[1]); }
+			$depart = ( $page - 1 ) * $imagePerPage;
+			$gallery = $this->galleryModel->getAllEditedImages( $depart, $imagePerPage );
+			$totalImages = $this->galleryModel->getCountImages();
 
-		// 	$this->call_view(
-		// 		'gallery' . DIRECTORY_SEPARATOR . 'gallery',
-		// 		[
-		// 			'success' => "true",
-		// 			'data' => [
-		// 				'page' => $page,
-		// 				'totalImages' => $totalImages,
-		// 				'userData' => $userData,	
-		// 				'gallery' => $gallery	
-		// 			]
-		// 		]
-		// 	)->render();
-		// }
+			$this->call_view(
+				'gallery' . DIRECTORY_SEPARATOR . 'gallery',
+				[
+					'success' => "true",
+					'data' => [
+						'page' => $page,
+						'totalImages' => $totalImages,
+						'gallery' => $gallery,
+						'userGallery' => $userGallery,
+						'userData' => $userData
+					]
+				]
+			)->render();
+		}
 
 		public function		notfound()
 		{
-			echo "404";
+			$this->call_view( 'notfound')->render();
 		}
 
 	}
