5b2d4ff58816de448e2a2f29621b49a9d339c75d
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-09-01 14:08:28 +0100

    update gallery model

--- app/models/GalleryModel.php
+++ app/models/GalleryModel.php
@@ -1,6 +1,11 @@
 <?php
+	
+	/**
+	 *  Gallery model
+	 */
 	class       GalleryModel extends DB
 	{
+
 		public function         addImage ( $data )
 		{
 			$query = "INSERT INTO gallery (userid, src) VALUES (?, ?)";
@@ -42,40 +47,6 @@
 			$stt = $this->connect()->prepare($query);
 			$stt->execute();
 			return $stt->fetchAll(PDO::FETCH_ASSOC);
-		}
-
-		public function          getUsersLikeImage ( $imgid )
-		{
-			$query = '
-				SELECT u.id, u.username
-				FROM `likes` l inner join `users` u
-				ON l.userid = u.id
-				WHERE l.imgid = ?
-				ORDER BY l.createdat DESC
-			';
-			$stt = $this->connect()->prepare($query);
-			$stt->execute([ $imgid ]);
-			return $stt->fetchAll(PDO::FETCH_ASSOC);
-		}
-
-		public function          likeImage ( $imgid, $userid )
-		{
-			$query = '
-			    INSERT INTO `likes` (userid, imgid) values (?, ?);
-			    UPDATE `gallery` SET countlikes = countlikes + 1 WHERE id = ?;
-			';
-			$stt = $this->connect()->prepare($query);
-			return $stt->execute([ $userid, $imgid, $imgid ]);
-		}
-
-		public function          unlikeImage ( $imgid, $userid )
-		{
-			$query = '
-			    DELETE FROM likes WHERE imgid = ? AND userid = ?;
-			    UPDATE `gallery` SET countlikes = countlikes - 1 WHERE id = ?;
-			';
-			$stt = $this->connect()->prepare($query);
-			return $stt->execute([ $imgid, $userid, $imgid ]);
 		}
 
 		public function         deleteImage ( $imgid, $userid )
