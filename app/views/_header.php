b8b11f8988e9ca5c57dec5062e1e78c544341bcf
ayoub guismi <i.guismi@gmail.com>
2020-11-18 20:25:45 +0100

    Update the header

--- app/views/_header.php
+++ app/views/_header.php
@@ -1,59 +1,59 @@
 <nav>
-	<div class="logo">
-		<img class="logo" src="<?php echo PUBLIC_FOLDER; ?>/images/logo.png" alt="logo-camagru"/>
+	<div class="logo-area">
+		<a href="/">
+			<img class="logo" src="/public/images/logo.png" alt="logo-camagru"/>
+		</a>
 	</div>
 	<ul class="nav-links">
-		<li><a class="active-link" href="<?php echo SERVER . '/home'; ?>">Home</a></li>
-		<li><a href="<?php echo SERVER . '/user/editing'; ?>">Editing</a></li>
-		<?php if ( isset($data['gallery']) ) {
-				if ( count( $data['gallery'] ) !== 0 ) {
-		?>
-			<li><a href="<?php echo SERVER . '/gallery/index'; ?>">Gallery</a></li>
-		<?php } } ?>
-		<li><a href="<?php echo SERVER . '/help'; ?>">Help</a></li>
-		<li><a href="<?php echo SERVER . '/aboutus'; ?>">About us</a></li>
-		<li><a href="<?php echo SERVER . '/signin'; ?>" id="btn-signin">Signin</a></li>
-		<li><a href="<?php echo SERVER . '/signup'; ?>" id="btn-signup">Signup</a></li>
+		<li class="active-link"><a href="/">Home</a></li>
+		<li><a href="/user/editing">Editing</a></li>
+		<?php if ( isset($data['gallery']) && count( $data['gallery'] ) !== 0 ) { ?>
+			<li><a href="/gallery">Gallery</a></li>
+		<?php } ?>
+		<li><a href="/help">Help</a></li>
+		<li><a href="/aboutus">About us</a></li>
+		<li><a href="/signin" id="btn-signin">Signin</a></li>
+		<li><a href="/signup" id="btn-signup">Signup</a></li>
 	</ul>
 	<div class="btn-auth">
 		<?php if ( !isset($_SESSION['userid']) ) { ?>
-			<a href="<?php echo SERVER; ?>/signin" id="btn-signin">Signin</a>
-			<a href="<?php echo SERVER; ?>/signup" id="btn-signup">Signup</a>
+			<a href="/signin" id="btn-signin">Signin</a>
+			<a href="/signup" id="btn-signup">Signup</a>
 		<?php } else { ?>
-			<img src="<?php echo PUBLIC_FOLDER; ?>/images/notification.png"? id="notif-img">
+			<img src="/public/images/notification.png"? id="notif-img">
 			<?php if ( $userData['gender'] === "female" ) { ?>
-				<img src="<?php echo PUBLIC_FOLDER; ?>/images/user-female.png"? id="profile-img" onclick="showMenu()">
+				<img src="/public/images/user-female.png"? id="profile-img" onclick="showMenu()">
 			<?php } else { ?>
-				<img src="<?php echo PUBLIC_FOLDER; ?>/images/user-male.png"? id="profile-img" onclick="showMenu()">
+				<img src="/public/images/user-male.png"? id="profile-img" onclick="showMenu()">
 			<?php } ?>
 			<div class="dropdown">
 				<ul>
 					<?php if ( isset( $userGallery ) && !empty( $userGallery ) ) { ?>
 						<li>
-							<a href="<?php echo SERVER; ?>/gallery/user/username/<?php echo $userData['username'] ?>">
-								<img src="<?php echo PUBLIC_FOLDER; ?>/images/gallery-icone.png" id="gallery"/> My gallery
+							<a href="/gallery/user/username/<?php echo $userData['username'] ?>">
+								<img src="/public/images/gallery-icone.png" id="gallery"/> My gallery
 							</a>
 						</li>
 					<?php } ?>
 					<li>
-						<a href="<?php echo SERVER; ?>/user/profile">
+						<a href="/user/profile">
 							<?php if ( $userData['gender'] ==="female" ) { ?>
-								<img src="<?php echo PUBLIC_FOLDER; ?>/images/profile-female.png" id="profile"/>
+								<img src="/public/images/profile-female.png" id="profile"/>
 							<?php } else { ?>
-								<img src="<?php echo PUBLIC_FOLDER; ?>/images/profile-male.png" id="profile"/>
+								<img src="/public/images/profile-male.png" id="profile"/>
 							<?php } ?>
 							Profile
 						</a>
 					</li>
 					<li>
-						<a href="<?php echo SERVER; ?>/user/settings">
-							<img src="<?php echo PUBLIC_FOLDER; ?>/images/settings.png" id="settings"/>
+						<a href="/user/settings">
+							<img src="/public/images/settings.png" id="settings"/>
 							Settings
 						</a>
 					</li>
 					<li>
-						<a href="<?php echo SERVER; ?>/user/logout">
-							<img src="<?php echo PUBLIC_FOLDER; ?>/images/logout.png" id="logout"/>
+						<a href="/user/logout">
+							<img src="/public/images/logout.png" id="logout"/>
 							Logout
 						</a>
 					</li>
