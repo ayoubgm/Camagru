a9f828999cd39866932a67b48ba003450d8411d2
ayoub guismi <i.guismi@gmail.com>
2020-11-18 20:26:04 +0100

    Update home index view

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -11,17 +11,17 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Camagru</title>
-	<link rel="stylesheet" href="<?php echo PUBLIC_FOLDER; ?>/css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="<?php echo PUBLIC_FOLDER; ?>/css/index.css"/>
-	<link rel="stylesheet" href="<?php echo PUBLIC_FOLDER; ?>/css/_header.css"/>
-	<link rel="stylesheet" href="<?php echo PUBLIC_FOLDER; ?>/css/_footer.css"/>
+	<link rel="stylesheet" href="/public/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="/public/css/index.css"/>
+	<link rel="stylesheet" href="/public/css/_header.css"/>
+	<link rel="stylesheet" href="/public/css/_footer.css"/>
 </head>
 <body>
 	<?php require_once(VIEWS . "_header.php"); ?>
 	<div class="jumbotron">
-		<div class="container col-lg-6">
-			<h1 class="display-4">
-				<?php echo ( isset($_SESSION['userid']) ) ? "Welcome" : "Camagru"; ?>
+		<div class="container col-lg-8">
+			<h1 class="display-3">
+				<?php echo ( isset($_SESSION['userid']) ) ? "Welcome" : ""; ?>
 			</h1>
 			<p class="lead">Make your pictures looks nice with our editor.</p>
 			<hr class="my-4">
