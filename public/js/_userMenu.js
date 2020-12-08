43528cf2bae4999424b74ba66cabaab8ce414619
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-12-08 13:15:22 +0100

    Add js file for user menui

---
+++ public/js/_userMenu.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector("nav .btn-auth .dropdown");
+const btnProfilePic = document.getElementById("profile-img");
+
+document.addEventListener('click', (event) => {
+	var isClickedInsideMenu = menu.contains(event.target);
+	var isClickedInsideProfilePic = btnProfilePic.contains(event.target);
+		
+	if ( isClickedInsideProfilePic && menu.style.display == "none" ) {
+		menu.style.display = "block";
+	} else if ( !isClickedInsideMenu ) {
+		menu.style.display = "none";
+	}
+});
