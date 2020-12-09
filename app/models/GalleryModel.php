0ee713d7b8b75cfb7a95dedb18b845c144bee54e
ayoub guismi <i.guismi@gmail.com>
2020-12-09 20:18:08 +0100

    Update gallery model

--- app/models/GalleryModel.php
+++ app/models/GalleryModel.php
@@ -22,8 +22,8 @@
 			    SELECT g.*, u.username, u.gender
 			    FROM `gallery` g INNER JOIN `users` u
 			    ON g.userid = u.id
-			    WHERE username = ?
-			    ORDER BY createdat DESC LIMIT '.$depart.','.$imagePerPage;
+			    WHERE u.username = ?
+			    ORDER BY g.createdat DESC LIMIT '.$depart.','.$imagePerPage;
 			$stt = $this->connect()->prepare($query);
 			$stt->execute([ strtolower( $username ) ]);
 			return $stt->fetchAll(PDO::FETCH_ASSOC);
