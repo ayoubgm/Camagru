cc7684aae0dc8e607672725cdce5020d613cea0f
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-12-10 17:49:01 +0100

    Update user middleware

--- app/middlewares/UserMiddleware.php
+++ app/middlewares/UserMiddleware.php
@@ -6,32 +6,44 @@
 	class UserMiddleware extends Middleware
 	{
 
-		// Middleware for validating register data
-		public function      signup ( $data )
-		{
-			if ( !$data['firstname'] || !$data['lastname'] || !$data['username'] || !$data['email'] || !$data['password'] || !$data['confirmation_password'] ) { return "Invalid data provided !"; }
-			else if ( $error = $this->validateFirstname( $data['firstname'] ) ) { return $error; }
-			else if ( $error = $this->validateLastname( $data['lastname'] ) ) { return $error; }
-			else if ( $error = $this->validateUsername( $data['username'] ) ) { return $error; }
-			else if ( $error = $this->validateEmail( $data['email'] ) ) { return $error; }
-			else if ( $error = $this->validateGender( $data['gender'] ) ) { return $error; }
-			else if ( $error = $this->validateAddress( $data['address'] ) ) { return $error; }
-			else if ( $error = $this->validatePassword( $data['password'] ) ) { return $error; }
-			else if ( $data['password'] != $data['confirmation_password'] ) { return "Passwords doesn't match !"; }
-			else if ( $this->isFullnameExists( strtolower( $data['firstname'] ), strtolower( $data['lastname'] ) ) ) { return "The fullname is already exists !"; }
-			else if ( $this->isUsernameExists( strtolower( $data['username']) ) ) { return "The username is already exists !"; }
-			else if ( $this->isEmailExists( strtolower( $data['email']) ) ) { return "The email is already exists !"; }
-			else { return null; }
-		}
-
 		// Middleware for validating sign in data
 		public function      signin ( $data )
 		{
-			if ( !$data['username'] || !$data['password'] ) { return "Invalid data provided !"; }
-			else if ( !$this->isUsernameExists( strtolower( $data['username']) ) ) { return "The username does't exists !"; }
-			else if ( !$this->isActiveAccount( strtolower( $data['username' ]) ) ) { return "You must activate your account first !"; }
-			else if ( !$this->isThePasswordIsValid( null, strtolower( $data['username' ]), $data['password'] ) ) { return "Incorrect password !"; }
-			else { return null; }
+			if ( !$data['username'] || !$data['password'] ) {
+				return "Invalid data provided !";
+			} else if ( !$this->isUsernameExists( strtolower( $data['username']) ) ) {
+				return "The account does't exists !";
+			} else if ( !$this->isActiveAccount( strtolower( $data['username' ]) ) ) {
+				return "You must activate your account first !";
+			} else if ( !$this->isThePasswordIsValid( null, strtolower( $data['username' ]), $data['password'] ) ) {
+				return "Incorrect password !";
+			}
+		}
+
+		// Middleware for validating register data
+		public function      signup ( $data )
+		{
+			if ( !$data['firstname'] || !$data['lastname'] || !$data['username'] || !$data['email'] || !$data['password'] || !$data['confirmation_password'] ) {
+				return "Invalid data provided !";
+			} else if (
+				( $error = $this->validateFirstname( $data['firstname'] ) ) ||
+				( $error = $this->validateLastname( $data['lastname'] ) ) ||
+				( $error = $this->validateUsername( $data['username'] ) ) ||
+				( $error = $this->validateEmail( $data['email'] ) ) ||
+				( $error = $this->validateGender( $data['gender'] ) ) ||
+				( $error = $this->validateAddress( $data['address'] ) ) ||
+				( $error = $this->validatePassword( $data['password'] ) )
+			) {
+				return $error;
+			} else if ( $data['password'] != $data['confirmation_password'] ) {
+				return "Passwords doesn't match !";
+			} else if ( $this->isFullnameExists( strtolower( $data['firstname'] ), strtolower( $data['lastname'] ) ) ) {
+				return "The fullname is already exists !";
+			} else if ( $this->isUsernameExists( strtolower( $data['username']) ) ) {
+				return "The username is already exists !";
+			} else if ( $this->isEmailExists( strtolower( $data['email']) ) ) {
+				return "The email is already exists !";
+			}
 		}
 		
 		// Middleware for validating edited data
