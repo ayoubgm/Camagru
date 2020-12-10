dc496de8610cfea2454fd4a370efb3da28461e9a
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2020-12-10 17:49:23 +0100

    Update js file of user manu

--- public/js/_userMenu.js
+++ public/js/_userMenu.js
@@ -2,12 +2,14 @@ const menu = document.querySelector("nav .btn-auth .dropdown");
 const btnProfilePic = document.getElementById("profile-img");
 
 document.addEventListener('click', (event) => {
-	var isClickedInsideMenu = menu.contains(event.target);
-	var isClickedInsideProfilePic = btnProfilePic.contains(event.target);
-		
-	if ( isClickedInsideProfilePic && menu.style.display == "none" ) {
-		menu.style.display = "block";
-	} else if ( !isClickedInsideMenu ) {
-		menu.style.display = "none";
-	}
+	if ( menu && btnProfilePic ) {
+		var isClickedInsideMenu = menu.contains(event.target);
+		var isClickedInsideProfilePic = btnProfilePic.contains(event.target);
+
+		if ( isClickedInsideProfilePic && menu.style.display == "none" ) {
+			menu.style.display = "block";
+		} else if ( !isClickedInsideMenu ) {
+			menu.style.display = "none";
+		}
+	}	
 });
