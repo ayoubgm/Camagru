467e672704bf6a76e366014e62eb7698833e9370
ayoub guismi <i.guismi@gmail.com>
2020-12-15 20:29:14 +0100

    Add comment middleware

---
+++ app/middlewares/CommentMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+	/**
+	 * 	gallery middlewares class 
+	 */
+	class CommentMiddleware extends Middleware
+	{
+
+		public function         add ( $comment )
+		{
+			if ( empty( $comment ) ) { return "Should not be empty !"; }
+			else if ( strlen( $comment ) > 255 ) { return "The comment is too long !"; }
+			else if ( !preg_match( "/^[a-zA-Z0-9 ]{1,255}$/", $comment ) ) { return "The comment should contains only letters or numbers !"; }
+			else { return NULL; }
+		}
+
+	}
+
+?>
