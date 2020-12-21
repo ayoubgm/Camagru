5dc49fdfdc39d8471f24bad98f4bbf8b80a02049
ayoub guismi <i.guismi@gmail.com>
2020-12-21 14:49:36 +0100

    handle the user token on notification controller

--- app/controllers/NotificationController.php
+++ app/controllers/NotificationController.php
@@ -7,6 +7,7 @@
 	class notificationController extends Controller {
 		
 		private $viewData;
+		private $userMiddleware;
 		private $notificationModel;
 		private $helper;
 
@@ -15,6 +16,7 @@
 			session_start();
 			$this->viewData = array();
 			$this->notificationModel = self::call_model("NotificationsModel");
+			$this->userMiddleware = self::call_middleware('UserMiddleware');
 			$this->helper = self::call_helper();
 		}
 
@@ -43,14 +45,16 @@
 			if ( !isset( $_SESSION['userid'] ) ) {
 				$this->viewData = [ "success" => "false", "msg" => "You need to login first !" ];	
 			} else {
-				try {
-					if ( !$this->notificationModel->readUserNotifications( $_SESSION['userid'] ) ) {
-						$this->viewData = [ "success" => "false", "msg" => "Failed to read your notifications !" ];
-					} else {
-						$this->viewData = [ "success" => "true", "msg" => "Read all your notifications successfully !" ];
+				if ( ( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) && $this->userMiddleware->validateUserToken( $_POST["token"] ) ) {
+					try {
+						if ( !$this->notificationModel->readUserNotifications( $_SESSION['userid'] ) ) {
+							$this->viewData = [ "success" => "false", "msg" => "Failed to read your notifications !" ];
+						} else {
+							$this->viewData = [ "success" => "true", "msg" => "Read all your notifications successfully !" ];
+						}
+					} catch ( Exception $e ) {
+						$this->viewData = [ "success" => "false", "msg" => "Something went wrong while read your notifications !" ];
 					}
-				} catch ( Exception $e ) {
-					$this->viewData = [ "success" => "false", "msg" => "Something went wrong while read your notifications !" ];
 				}
 			}
 			die( json_encode( $this->viewData ) );
@@ -62,14 +66,16 @@
 			if ( !isset( $_SESSION['userid'] ) ) {
 				$this->viewData = [ "success" => "false", "msg" => "You need to login first !" ];	
 			} else {
-				try {
-					if ( !$this->notificationModel->deleteUserNotifications( $_SESSION['userid'] ) ) {
-						$this->viewData = [ "success" => "false", "msg" => "Failed to delete your notifications !" ];
-					} else {
-						$this->viewData = [ "success" => "true", "msg" => "Your notifications was deleted successfully !" ];
+				if ( ( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) && $this->userMiddleware->validateUserToken( $_POST["token"] ) ) {
+					try {
+						if ( !$this->notificationModel->deleteUserNotifications( $_SESSION['userid'] ) ) {
+							$this->viewData = [ "success" => "false", "msg" => "Failed to delete your notifications !" ];
+						} else {
+							$this->viewData = [ "success" => "true", "msg" => "Your notifications was deleted successfully !" ];
+						}
+					} catch ( Exception $e ) {
+						$this->viewData = [ "success" => "false", "msg" => "Something went wrong while delete your notifications !" ];
 					}
-				} catch ( Exception $e ) {
-					$this->viewData = [ "success" => "false", "msg" => "Something went wrong while delete your notifications !" ];
 				}
 			}
 			die( json_encode( $this->viewData ) );
