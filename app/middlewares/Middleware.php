9ce7bca8ca88d75e576265fc57b3aea9c80db8d6
ayoub guismi <i.guismi@gmail.com>
2020-12-22 20:35:17 +0100

    Fix the problems of models

--- app/middlewares/Middleware.php
+++ app/middlewares/Middleware.php
@@ -5,63 +5,63 @@
 	 */
 	class Middleware extends DB {
 
-		protected function		validateFirstname ( $firstname )
+		public function				validateFirstname ( $firstname )
 		{
 			return ( !preg_match( "/^[a-z]{3,30}$/", strtolower( $firstname ) ) )
 			? "The firstname must contains letters only ( between 3 and 30 ) !"
 			: null;
 		}
 
-		protected function		validateLastname ( $lastname )
+		public function				validateLastname ( $lastname )
 		{
 			return ( !preg_match( "/^[a-z]{3,30}$/", strtolower( $lastname ) ) )
 			? "The lastname must contains letters only ( between 3 and 30 ) !"
 			: null;
 		}
 
-		protected function		validateUsername ( $username )
+		public function				validateUsername ( $username )
 		{
 			return ( !preg_match( "/^(?=.{3,20}$)(?![-_.])(?!.*[-_.]{2})[a-z0-9._-]+(?<![-_.])$/", strtolower( $username ) ) )
 			? "The username should contain between 3 and 20 letters or numbers ( -, _ or . ) !"
 			: null;
 		}
 
-		protected function		validateEmail ( $email )
+		public function				validateEmail ( $email )
 		{
 			return ( !preg_match( "/[a-z0-9-_.]{1,50}@[a-z0-9-_.]{1,50}\.[a-z0-9]{2,10}$/", strtolower( $email ) ) )
 			? "Invalid email address !"
 			: null;
 		}
 
-		protected function		validateGender ( $gender )
+		public function				validateGender ( $gender )
 		{
 			return ( strtolower($gender) == "male" || strtolower($gender) == "female" )
 			? null
 			: "The gender should be either male or female !";
 		}
 
-		protected function		validateAddress ( $address )
+		public function				validateAddress ( $address )
 		{
 			return ( !preg_match("/^[a-z0-9\s,'-]*$/", strtolower($address)) )
 			? "The address should be contains letters or numbers ( ',', ' or - ) !"
 			: null;
 		}
 
-		protected function		validatePassword ( $password )
+		public function				validatePassword ( $password )
 		{
 			return ( !preg_match( "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*_-]).{8,}$/", $password ) )
 			? "The password should be minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character !"
 			: null;
 		}
 
-		protected function		validateOldPassword ( $oldpassword )
+		public function				validateOldPassword ( $oldpassword )
 		{
 			return ( $this->validatePassword( $oldpassword ) )
 			? "The old password should be minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character !"
 			: null;
 		}
 
-		protected function		validateNewPassword ( $newpassword )
+		public function				validateNewPassword ( $newpassword )
 		{
 			return ( $this->validatePassword( $newpassword ) )
 			? "The new password should be minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character !"
@@ -69,111 +69,95 @@
 		}
 		
 
-		protected function		isFullnameExists ( $firstname, $lastname )
+		public function				isFullnameExists ( $firstname, $lastname )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE firstname = ? AND lastname = ?");
-			$stt->execute([ strtolower($firstname), strtolower($lastname) ]);
+			$stt = $this->query("SELECT * FROM `users` WHERE firstname = ? AND lastname = ?", [ strtolower($firstname), strtolower($lastname) ]);
 			return( $stt->fetch() );
 		}
 
-		protected function		isUsernameExists ( $username )
+		public function				isUsernameExists ( $username )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE username = ?");
-			$stt->execute([ strtolower( $username ) ]);
+			$stt = $this->query("SELECT * FROM `users` WHERE username = ?", [ strtolower( $username ) ]);
 			return( $stt->fetch() );
 		}
 
-		protected function		isEmailExists ( $email )
+		public function				isEmailExists ( $email )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE email = ?");
-			$stt->execute([ strtolower( $email ) ]);
+			$stt = $this->query("SELECT * FROM `users` WHERE email = ?", [ strtolower( $email ) ]);
 			return( $stt->fetch() );
 		}
 
-		protected function		isActiveAccount ( $username )
+		public function				isActiveAccount ( $username )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE username = ?");
-			$stt->execute([ strtolower( $username ) ]);
-			$userData = $stt->fetch();
-			
-			return ( empty($userData['activationToken']) );
+			$stt = $this->query("SELECT * FROM `users` WHERE username = ?", [ strtolower( $username ) ]);
+			$data = $stt->fetch();
+			return ( empty($data['activationToken']) );
 		}
 
-		protected function		isThePasswordIsValid( $id, $username, $password )
+		public function				isThePasswordIsValid( $id, $username, $password )
 		{
-			if ( $id ) {
-				$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE id = ?");
-				$stt->execute([ $id ]);
-			} else if ( $username ){
-				$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE username = ?");
-				$stt->execute([ strtolower( $username ) ]);
-			}
-			$userData = $stt->fetch();
-			return ( password_verify($password, $userData['password']) );
+			$stt = ( $id )
+			? $this->query("SELECT * FROM `users` WHERE id = ?", [ $id ])
+			: $this->query("SELECT * FROM `users` WHERE username = ?", [ strtolower( $username ) ]);
+			$data = $stt->fetch();
+			return ( password_verify($password, $data['password']) );
 		}
 
-		protected function		isUsernameEditedExists ( $userid, $username )
+		public function				isUsernameEditedExists ( $userid, $username )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE username = ? AND id <> ?");
-			$stt->execute([ strtolower( $username ), $userid ]);
+			$stt = $this->query("SELECT * FROM `users` WHERE username = ? AND id <> ?", [ strtolower( $username ), $userid ]);
 			return( $stt->fetch() );
 		}
 
-		protected function		isEmailEditedExists ( $userid, $email )
+		public function				isEmailEditedExists ( $userid, $email )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE email = ? AND id <> ?");
-			$stt->execute([ strtolower( $email ), $userid ]);
+			$stt = $this->query("SELECT * FROM `users` WHERE email = ? AND id <> ?", [ strtolower( $email ), $userid ]);
 			return( $stt->fetch() );
 		}
 
-		protected function		isRecoveryTokenValid ( $token )
+		public function				isRecoveryTokenValid ( $token )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE recoveryToken = ?");
-			$stt->execute([ $token ]);
+			$stt = $this->query("SELECT * FROM `users` WHERE recoveryToken = ?", [ $token ]);
 			$data = $stt->fetch(PDO::FETCH_ASSOC);
 			return ( $data ) ? array( 'id' => $data['id'] ) : null;
 		}
 
-		public function			isActivationTokenValid ( $token )
+		public function				isActivationTokenValid ( $token )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `users` WHERE activationToken = ?");
-			$stt->execute([ $token ]);
+			$stt = $this->query("SELECT * FROM `users` WHERE activationToken = ?", [ $token ]);
 			$data = $stt->fetch(PDO::FETCH_ASSOC);
 			return ( $data ) ? array( 'id' => $data['id'] ) : null;
 		}
 
-		protected function			isImageOwnerExists ( $userid, $imgid )
+		public function				isImageOwnerExists ( $userid, $imgid )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `gallery` WHERE id = ? AND userid = ?");
-			$stt->execute([ $imgid, $userid ]);
+			$stt = $this->query("SELECT * FROM `gallery` WHERE id = ? AND userid = ?", [ $imgid, $userid ]);
 			$data = $stt->fetch(PDO::FETCH_ASSOC);
 			return ( $data ) ? $data : null;
 		}
 
-		protected function			isImageExists ( $imgid )
+		public function				isImageExists ( $imgid )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `gallery` WHERE id = ?");
-			$stt->execute([ $imgid ]);
+			$stt = $this->query("SELECT * FROM `gallery` WHERE id = ?", [ $imgid ]);
 			$data = $stt->fetch(PDO::FETCH_ASSOC);
 			return ( $data ) ? $data : null;
 		}
 
-		public function						validateImageDescription ( $descr )
+		public function				validateImageDescription ( $descr )
 		{
 			return ( !preg_match( "/^[A-Za-z0-9 ]{10,200}$/", $descr ) )
 			? "The description must contains letters and numbers only ( between 10 and 200 ) !"
 			: null;
 		}
 
-		public function						isCommentExists ( $id )
+		public function				isCommentExists ( $id )
 		{
-			$stt = $this->connect()->prepare("SELECT * FROM `comments` WHERE id = ?");
-			$stt->execute([ $id ]);
+			$stt = $this->query("SELECT * FROM `comments` WHERE id = ?", [ $id ]);
 			$data = $stt->fetch(PDO::FETCH_ASSOC);
 			return ( $data ) ? $data : null;
 		}
 
-		public function						isUserTokenValid ( $token )
+		public function				isUserTokenValid ( $token )
 		{
 			return ( hash_equals( $_SESSION["token"], $token ) ) ? true : false; 
 		}
