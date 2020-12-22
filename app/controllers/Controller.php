97fe51e4bb7ade15d85bce6816d8e32ae10cba17
ayoub guismi <i.guismi@gmail.com>
2020-12-22 20:35:41 +0100

    Updates

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -5,22 +5,22 @@
 	 */
 	class Controller {
 		
-		static protected function				call_model ( $model )
+		protected function				call_model ( $model )
 		{
 			if ( file_exists( MODELS . $model . '.php') ) { return new $model(); }
 		}
 
-		static protected function				call_view ( $view_name, $view_data = [] )
+		protected function				call_view ( $view_name, $view_data = [] )
 		{
 			if ( file_exists(VIEWS . $view_name . '.php') ) {  return new View( $view_name, $view_data ); }
 		}
 
-		static protected function				call_middleware ( $middleware )
+		protected function				call_middleware ( $middleware )
 		{
 			if ( file_exists( MIDDLEWARES . $middleware . '.php') ) { return new $middleware(); }
 		}
 
-		static protected function				call_helper ()
+		protected function				call_helper ()
 		{
 			if ( file_exists( HELPERS . "helper.php" ) ) { return new helper(); }
 		}
