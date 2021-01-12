f94e9905b30804749e9a747986484164b1f1df89
ayoub guismi <i.guismi@gmail.com>
2021-01-12 11:49:06 +0100

    Add construct method to set the necessary varibales to use on pdo instance

--- config/database.php
+++ config/database.php
@@ -4,18 +4,31 @@
 	 */
 	class		database {
 
-		private static $HOST = "localhost";
-		private static $USER = "root";
-		private static $PASSWORD = "root";
-		private static $DB_NAME = "db_camagru";
+		private $DSN;
+		private $USER;
+		private $PASSWORD;
+		private $DB_NAME;
 		protected $pdo;
+
+		public function						__construct()
+		{
+			$this->DB_NAME = "db_camagru";
+			$this->DSN = "mysql:host=" . $_SERVER["HTTP_HOST"] . ";dbname=". $this->DB_NAME . ";charset=utf8";
+			$this->USER = "root";
+			$this->PASSWORD = "tiger";
+		}
 
 		public function						connect()
 		{
 			try
 			{
-				$this->pdo = new PDO("mysql:host=".self::$HOST.";dbname=".self::$DB_NAME.";charset=utf8", self::$USER, self::$PASSWORD);
-				$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				if ( $this->pdo ) {
+					return $this->pdo;
+				} else {
+					$this->pdo = new PDO( $this->DSN, $this->USER, $this->PASSWORD );
+					$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					return $this->pdo;
+				}
 			} catch( PDOException $e ) {
 				echo "Something went wrong while connect to database (".$e->getMessage().")";
 			}
@@ -24,9 +37,9 @@
 		protected function					query( $query, $params = [] )
 		{
 			try {
-				if ( $this->pdo ) {
-					$stt = $this->pdo->prepare( $query );
-					return ( $stt->execute( $params ) ) ? $stt : NULL;
+				$stmt = $this->pdo->prepare( $query );
+				if ( $stmt->execute( $params ) ) {
+					return $stmt;
 				}
 			} catch( PDOException $e ) {
 				echo "Something went wrong while execute the query (".$e->getMessage().")";
