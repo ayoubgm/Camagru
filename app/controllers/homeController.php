0bc4ea39852c7cc7700e7f68e185c51a5c0d8459
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2021-01-12 23:50:32 +0100

    Complete validate home controller still must add reset password mail

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -5,31 +5,20 @@
 	 */
 	class homeController extends Controller {
 
-		private $viewData;
-		private $userMiddleware;
-		private $userModel;
-		private $galleryModel;
-		private $notificationsModel;
-
 		public function 				__construct()
 		{
-			session_start();
-			$this->viewData = array();
-			$this->userMiddleware = $this->call_middleware('UserMiddleware');
-			$this->userModel = $this->call_model('UsersModel');
-			$this->galleryModel = $this->call_model('GalleryModel');
-			$this->notificationsModel = $this->call_model('NotificationsModel');
-			$this->helper = $this->call_helper();
+			parent::__construct();
 		}
 
 		public function 				index()
 		{
+			session_start();
 			try {
-				$this->viewData["data"][ "gallery"] = $this->galleryModel->getAllEditedImages();
-				if ( $this->userMiddleware->isSignin( $_SESSION ) ) {
+				$this->viewData["data"][ "gallery"] = $this->gallery_model->getAllEditedImages();
+				if ( $this->user_middleware->isSignin( $_SESSION ) ) {
 					$this->viewData["data"] += [
-						"userData" => $this->userModel->findUserById( $_SESSION['userid'] ),
-						"countUnreadNotifs" => $this->notificationsModel->getCountUnreadNotifications( $_SESSION['userid'] )
+						"userData" => $this->user_model->findUserById( $_SESSION['userid'] ),
+						"countUnreadNotifs" => $this->notifications_model->getCountUnreadNotifications( $_SESSION['userid'] )
 					];
 				}
 				$this->viewData += [ "success" => "true" ];
@@ -42,21 +31,25 @@
 
 		public function 				signin()
 		{
+			session_start();
 			try {
-				if ( $this->userMiddleware->isSignin( $_SESSION ) ) {
+				if ( $this->user_middleware->isSignin( $_SESSION ) ) {
 					header("Location: /");
 				} else {
 					switch ( $_SERVER["REQUEST_METHOD"] ) {
 						case "GET":
-							$this->viewData = [ "success" => "true" ];			
+							$this->viewData["success"] = "true";			
 						break;
 						case "POST":
 							if ( isset( $_POST["btn-signin"] ) ) {
 								unset( $_POST["btn-signin"] );
-								if ( $error = $this->userMiddleware->signin( $_POST ) ) {
-									$this->viewData = [ "success" => "false", "msg" => $error ];
+								if ( $error = $this->user_middleware->signin( $_POST ) ) {
+									$this->viewData = [
+										"success" => "false",
+										"msg" => $error
+									];
 								} else {
-									$userData = $this->userModel->findUserByUsername( $_POST["username"] );
+									$userData = $this->user_model->findUserByUsername( $_POST["username"] );
 									$_SESSION = [
 										"userid" => $userData["id"],
 										"username" => $userData["username"],
@@ -69,15 +62,19 @@
 					}
 				}
 			} catch ( Exception $e ) {
-				$this->viewData = [ "success" => "false", "msg" => "Couldn't login, try later !" ];
+				$this->viewData = [
+					"success" => "false",
+					"msg" => "Couldn't login, try later !"
+				];
 			}
 			$this->call_view( 'home' . DIRECTORY_SEPARATOR .'signin', $this->viewData )->render();
 		}
 		
 		public function 				signup()
 		{
+			session_start();
 			try {
-				if ( $this->userMiddleware->isSignin( $_SESSION ) ) {
+				if ( $this->user_middleware->isSignin( $_SESSION ) ) {
 					header("Location: /");
 				} else {
 					switch( $_SERVER["REQUEST_METHOD"] ) {
@@ -85,12 +82,15 @@
 							$this->viewData = [ "success" => "true" ];			
 						break;
 						case "POST":
-							if ( isset($_POST["btn-signup"]) ) {
+							if ( isset( $_POST["btn-signup"] ) && !empty( $_POST["btn-signup"] ) ) {
 								unset( $_POST["btn-signup"] );
-								if ( $error = $this->userMiddleware->signup($_POST) ) {
-									$this->viewData = [ "success" => "false", "msg" => $error ];
-								} else if ( $atoken = $this->userModel->save( $_POST ) ) {
-									$this->helper->sendMail("Confirmation mail", strtolower($_POST["email"]), $atoken);
+								if ( $error = $this->user_middleware->signup( $_POST ) ) {
+									$this->viewData = [
+										"success" => "false",
+										"msg" => $error
+									];
+								} else if ( $atoken = $this->user_model->save( $_POST ) ) {
+									$this->helper->sendMail("Confirmation mail", strtolower( $_POST["email"] ), $atoken);
 									$this->viewData = [
 										"success" => "true",
 										"msg" => "Successful registration, you will receive an email for activation account !"
@@ -116,8 +116,9 @@
 
 		public function 				reset_password()
 		{
+			session_start();
 			try {
-				if ( $this->userMiddleware->isSignin( $_SESSION ) ) {
+				if ( $this->user_middleware->isSignin( $_SESSION ) ) {
 					header("Location: /");
 				} else {
 					switch ( $_SERVER["REQUEST_METHOD"] ) {
@@ -125,21 +126,33 @@
 							$this->viewData = [ "success" => "true" ];			
 						break;
 						case "POST":
-							if ( isset( $_POST["btn-reset"] ) ) {
-								if ( $error = $this->userMiddleware->reset_password($_POST["email"]) ) {
-									$this->viewData = [ "success" => "false", "msg" => $error ];
-								} else if ( $this->userModel->resetpassword( $_POST["email"] ) ) {
-									// $this->sendMail("Reset password", strtolower($_POST["email"]));
-									$this->viewData = [ "success" => "true", "msg" => "A direct link for reset password has been sent successfully !" ];
+							if ( isset( $_POST["btn-reset"] ) && !empty( $_POST["btn-reset"] ) ) {
+								if ( $error = $this->user_middleware->reset_password( $_POST["email"] ) ) {
+									$this->viewData = [
+										"success" => "false",
+										"msg" => $error
+									];
+								} else if ( $rToken = $this->user_model->resetpassword( $_POST["email"] ) ) {
+									// $this->sendMail("Reset password", strtolower( $_POST["email"] ), $rToken);
+									$this->viewData = [
+										"success" => "true",
+										"msg" => "A direct link for reset password has been sent successfully !"
+									];
 								} else {
-									$this->viewData = [ "success" => "false", "msg" => "failed to reset your password !" ];
+									$this->viewData = [
+										"success" => "false",
+										"msg" => "failed to reset your password !"
+									];
 								}
 							}
 						break;
 					}
 				}
 			} catch ( Exception $e ) {
-				$this->viewData = [ "success" => "false", "msg" => "Something goes wrong while reseting your password, try later !" ];
+				$this->viewData = [
+					"success" => "false",
+					"msg" => "Something goes wrong while reseting your password, try later !"
+				];
 			}
 			$this->call_view('home' . DIRECTORY_SEPARATOR .'reset_password', $this->viewData)->render();
 		}
@@ -151,69 +164,103 @@
 			}
 		}
 
-		public function 				new_password( $data )
-		{
-			try {
-				if ( $this->userMiddleware->isSignin( $_SESSION ) ) {
-					header("Location: /");
-				} else if ( $error = $this->validateToken( $data ) ) {
-					$this->viewData = [ "success" => "false", "msg" => $error ];
-				} else {
-					switch ( $_SERVER["REQUEST_METHOD"] ) {
-						case "GET":
-							if ( $error = $this->userMiddleware->validateRecoveryToken( $data[1] ) ) {
-								$this->viewData = [ "success" => "false", "msg" => $error, "data" => [ "token" => $data[1] ] ];
-							} else {
-								$this->viewData = [ "success" => "true", "data" => [ "token" => $data[1] ] ];
-							}
-						break;
-						case "POST":
-							if ( isset( $_POST["btn-submit"] ) ) {
-								unset( $_POST["btn-submit"] ); $_POST["token"] = $data[1];
-								if ( $error = $this->userMiddleware->new_password( $_POST ) ) {
-									$this->viewData = [ "success" => "false", "msg" => $error, "data" => [ "token" => $data[1] ] ];
-								} else if ( $this->userModel->newpassword( array( "newpassword" => password_hash($_POST["newpassword"], PASSWORD_ARGON2I), "token" => $data[1] ) ) ) {
-									$this->viewData = [ "success" => "true", "msg" => "Your password has been changed successfully !", "data" => [ "token" => $data[1] ] ];
-								} else {
-									$this->viewData = [ "success" => "false", "msg" => "Failed to change your password !", "data" => [ "token" => $data[1] ] ];
-								}
-							}
-						break;
-					}
-				}
-			} catch ( Exception $e ) {
-				$this->viewData = ["success" => "false", "msg" => "Something goes wrong, try later !"];
-			}
-			$this->call_view( 'home' . DIRECTORY_SEPARATOR .'new_password', $this->viewData)->render();
-		}
-		
 		public function 				account_confirmation ( $data )
 		{
+			session_start();
 			try {
-				if ( $this->userMiddleware->isSignin( $_SESSION ) ) {
+				if ( $this->user_middleware->isSignin( $_SESSION ) ) {
 					header("Location: /");
 				} else {
 					switch ( $_SERVER["REQUEST_METHOD"] ) {
 						case "GET":
 							if ( $error = $this->validateToken( $data ) ) {
-								$this->viewData = [ "success" => "false", "msg" => $error ];
+								$this->viewData = [
+									"success" => "false",
+									"msg" => $error
+								];
 							} else {
 								$this->viewData[ "data" ] = [ "token" => $data[1] ];
-								if ( $error = $this->userMiddleware->validateActivationToken( $data[1] ) ) {
-									$this->viewData += [ "success" => "false", "msg" => $error ];
-								} else if ( $this->userModel->activateAccount( array( 'token' => $data[1] ) ) ) {
-									$this->viewData += [ "success" => "true", "msg" => "Your account has been activated successfully !" ];
+								if ( $error = $this->user_middleware->validateActivationToken( $data[1] ) ) {
+									$this->viewData += [
+										"success" => "false",
+										"msg" => $error
+									];
+								} else if ( $this->user_model->activateAccount( array( 'token' => $data[1] ) ) ) {
+									$this->viewData += [
+										"success" => "true",
+										"msg" => "Your account has been activated successfully !"
+									];
 								} else {
-									$this->viewData += [ "success" => "false", "msg" => "Failed to activate your account !" ];
+									$this->viewData += [
+										"success" => "false",
+										"msg" => "Failed to activate your account !"
+									];
 								}
 							}
 						break;
 					}
 				}
 			} catch ( Exception $e ) {
-				$this->viewData = [ "success" => "false", "msg" => "Something goes wrong while activating your account, try later !"];
+				$this->viewData = [
+					"success" => "false",
+					"msg" => "Something goes wrong while activating your account, try later !"
+				];
 			}
 			$this->call_view( 'home' . DIRECTORY_SEPARATOR .'account_confirmation', $this->viewData)->render();
+		}
+
+		public function 				new_password( $data )
+		{
+			session_start();
+			try {
+				if ( $this->user_middleware->isSignin( $_SESSION ) ) {
+					header("Location: /");
+				} else if ( $error = $this->validateToken( $data ) ) {
+					$this->viewData = [
+						"success" => "false",
+						"msg" => $error
+					];
+				} else {
+					$this->viewData["data"][ "token"] = $data[1];
+					switch ( $_SERVER["REQUEST_METHOD"] ) {
+						case "GET":
+							if ( $error = $this->user_middleware->validateRecoveryToken( $data[1] ) ) {
+								$this->viewData += [
+									"success" => "false",
+									"msg" => $error
+								];
+							} else {
+								$this->viewData += [ "success" => "true" ];
+							}
+						break;
+						case "POST":
+							if ( isset( $_POST["btn-submit"] ) ) {
+								unset( $_POST["btn-submit"] ); $_POST["token"] = $data[1];
+								if ( $error = $this->user_middleware->new_password( $_POST ) ) {
+									$this->viewData += [
+										"success" => "false",
+										"msg" => $error
+									];
+								} else if ( $this->user_model->newpassword( array( password_hash($_POST["newpassword"], PASSWORD_ARGON2I), $data[1] ) ) ) {
+									$this->viewData += [
+										"success" => "true",
+										"msg" => "Your password has been changed successfully !"
+									];
+								} else {
+									$this->viewData += [
+										"success" => "false",
+										"msg" => "Failed to change your password !"
+									];
+								}
+							}
+						break;
+					}
+				}
+			} catch ( Exception $e ) {
+				$this->viewData["success"] = "false";
+				$this->viewData["msg"] = "Something goes wrong, try later !";
+			}
+			$this->call_view( 'home' . DIRECTORY_SEPARATOR .'new_password', $this->viewData)->render();
 		}
 
 		public function 				notfound()
