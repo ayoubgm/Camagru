f4d4867be1d2dd39dcb990685afe710967afb182
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2021-01-13 00:30:02 +0100

    Update user controller

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -5,20 +5,10 @@
 	 */
 	class userController extends Controller {
 
-		private $viewData;
-		private $userMiddleware;
-		private $userModel;
-		private $galleryModel;
-		private $notificationsModel;
-
 		public function 				__construct ()
 		{
+			parent::__construct();
 			session_start();
-			$this->viewData = array();
-			$this->userMiddleware = $this->call_middleware('UserMiddleware');
-			$this->userModel = $this->call_model('UsersModel');
-			$this->galleryModel = $this->call_model('GalleryModel');
-			$this->notificationsModel = $this->call_model('NotificationsModel');
 		}
 
 		public function 				profile( $data )
@@ -28,19 +18,19 @@
 			switch( $_SERVER["REQUEST_METHOD"] ) {
 				case "GET":
 					try {
-						if ( !$this->userMiddleware->isSignin( $_SESSION ) ) {
+						if ( !$this->user_middleware->isSignin( $_SESSION ) ) {
 							$redirect = "signin";
 						} else {
 							$redirect = "profile";
 							$this->viewData["data"] = [
 								"success" => "true",
-								"gallery" => $this->galleryModel->getAllEditedImages(),
-								"countUnreadNotifs" => $this->notificationsModel->getCountUnreadNotifications( $_SESSION['userid'] )
+								"gallery" => $this->gallery_model->getAllEditedImages(),
+								"countUnreadNotifs" => $this->notifications_model->getCountUnreadNotifications( $_SESSION['userid'] )
 							];
 							if ( ( isset( $data[0] ) && $data[0] === "username" ) && ( isset( $data[1] ) && !empty( $data[1] ) ) ) {
-								$this->viewData["data"] += [ "userData" => $this->userModel->findUserByUsername( strtolower( $data[1] ) ) ];
+								$this->viewData["data"] += [ "userData" => $this->user_model->findUserByUsername( strtolower( $data[1] ) ) ];
 							} else {
-								$this->viewData["data"] += [ "userData" => $this->userModel->findUserById( $_SESSION["userid"] ) ];
+								$this->viewData["data"] += [ "userData" => $this->user_model->findUserById( $_SESSION["userid"] ) ];
 							}
 						}
 					} catch ( Exception $e ) {
@@ -56,30 +46,30 @@
 		public function 				edit()
 		{
 			try {
-				if ( !$this->userMiddleware->isSignin( $_SESSION ) ) {
+				if ( !$this->user_middleware->isSignin( $_SESSION ) ) {
 					header("Location: /");
 				} else {
 					$this->viewData["data"] = [
-						"gallery" => $this->galleryModel->getAllEditedImages(),
-						"userData" => $this->userModel->findUserById( $_SESSION["userid"] ),
-						"countUnreadNotifs" => $this->notificationsModel->getCountUnreadNotifications( $_SESSION['userid'] )
+						"gallery" => $this->gallery_model->getAllEditedImages(),
+						"userData" => $this->user_model->findUserById( $_SESSION["userid"] ),
+						"countUnreadNotifs" => $this->notifications_model->getCountUnreadNotifications( $_SESSION['userid'] )
 					];
-					$oldData = $this->userModel->findUserById( $_SESSION["userid"] );
+					$oldData = $this->user_model->findUserById( $_SESSION["userid"] );
 					switch( $_SERVER["REQUEST_METHOD"] ) {
 						case "GET":
 							$this->viewData[ "success" ] = "true";
 						break;
 						case "POST":
-							if ( ( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) && $this->userMiddleware->validateUserToken( $_POST["token"] ) && ( isset( $_POST["btn-edit"] ) && !empty( $_POST["btn-edit"] ) ) ) {
+							if ( ( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) && $this->user_middleware->validateUserToken( $_POST["token"] ) && ( isset( $_POST["btn-edit"] ) && !empty( $_POST["btn-edit"] ) ) ) {
 								unset( $_POST["btn-edit"] ); unset( $_POST["token"] );
 								unset( $oldData["id"] ); unset( $oldData["createdat"] ); unset( $oldData["notifEmail"] );
 								$editedData = array_replace_recursive( $oldData, $_POST );
-								if ( ($error = $this->userMiddleware->edit( $_SESSION["userid"], $editedData )) != null) {
+								if ( ($error = $this->user_middleware->edit( $_SESSION["userid"], $editedData )) != null) {
 									$this->viewData += [ "success" => "false", "msg" => $error ];
 									$this->viewData["data"]["userData"] = $editedData;
-								} else if ( $this->userModel->edit( $_SESSION["userid"], $editedData ) ) {
+								} else if ( $this->user_model->edit( $_SESSION["userid"], $editedData ) ) {
 									$this->viewData += [ "success" => "true", "msg" => "Your informations has been edited successfully !" ];
-									$this->viewData["data"]["userData"] = $this->userModel->findUserById( $_SESSION["userid"] );
+									$this->viewData["data"]["userData"] = $this->user_model->findUserById( $_SESSION["userid"] );
 								} else {
 									$this->viewData += [ "success" => "true", "msg" => "Failed to change your informations !" ];
 								}
@@ -100,14 +90,14 @@
 			switch( $_SERVER["REQUEST_METHOD"] ) {
 				case "GET":
 					try {
-						if ( !$this->userMiddleware->isSignin( $_SESSION ) ) {
+						if ( !$this->user_middleware->isSignin( $_SESSION ) ) {
 							header("Location: /");
 						} else {
 							$this->viewData["data"] = [
 								"success" => "true",
-								"userData" => $this->userModel->findUserById( $_SESSION["userid"] ),
-								"gallery" => $this->galleryModel->getAllEditedImages(),
-								"countUnreadNotifs" => $this->notificationsModel->getCountUnreadNotifications( $_SESSION['userid'] )
+								"userData" => $this->user_model->findUserById( $_SESSION["userid"] ),
+								"gallery" => $this->gallery_model->getAllEditedImages(),
+								"countUnreadNotifs" => $this->notifications_model->getCountUnreadNotifications( $_SESSION['userid'] )
 							];
 						}
 					} catch ( Exception $e ) {
@@ -122,24 +112,24 @@
 		public function 				change_password ()
 		{
 			try {
-				if ( !$this->userMiddleware->isSignin( $_SESSION ) ) {
+				if ( !$this->user_middleware->isSignin( $_SESSION ) ) {
 					header("Location: /");
 				} else {
 					$this->viewData["data"] = [
-						"gallery" => $this->galleryModel->getAllEditedImages(),
-						"userData" => $this->userModel->findUserById( $_SESSION["userid"] ),
-						"countUnreadNotifs" => $this->notificationsModel->getCountUnreadNotifications( $_SESSION['userid'] )
+						"gallery" => $this->gallery_model->getAllEditedImages(),
+						"userData" => $this->user_model->findUserById( $_SESSION["userid"] ),
+						"countUnreadNotifs" => $this->notifications_model->getCountUnreadNotifications( $_SESSION['userid'] )
 					];
 					switch( $_SERVER["REQUEST_METHOD"] ) {
 						case "GET":
 							$this->viewData["success"] = "true";
 						break;
 						case "POST":
-							if ( ( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) && $this->userMiddleware->validateUserToken( $_POST["token"] ) && ( isset( $_POST["btn-submit"] ) && !empty( $_POST["btn-submit"] ) ) ) {
+							if ( ( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) && $this->user_middleware->validateUserToken( $_POST["token"] ) && ( isset( $_POST["btn-submit"] ) && !empty( $_POST["btn-submit"] ) ) ) {
 								unset( $_POST["btn-submit"] );
-								if ( $error = $this->userMiddleware->change_password( $_SESSION["userid"], $_POST ) ) {
+								if ( $error = $this->user_middleware->change_password( $_SESSION["userid"], $_POST ) ) {
 									$this->viewData += [ "success" => "false", "msg" => $error ];
-								} else if ( $this->userModel->change_password( $_SESSION["userid"], password_hash($_POST["newpassword"], PASSWORD_ARGON2I) ) ) {
+								} else if ( $this->user_model->change_password( $_SESSION["userid"], password_hash($_POST["newpassword"], PASSWORD_ARGON2I) ) ) {
 									$this->viewData += [ "success" => "true", "msg" => "Your password has been changed successfully !" ];
 								} else {
 									$this->viewData += [ "success" => "false", "msg" => "Failed to your password !" ];
@@ -158,13 +148,13 @@
 		public function 				notifications_preferences ( $data )
 		{
 			try {
-				if ( !$this->userMiddleware->isSignin( $_SESSION ) ) {
+				if ( !$this->user_middleware->isSignin( $_SESSION ) ) {
 					header("Location: /home");
 				} else {
 					$this->viewData["data"] = [
-						"userData" => $this->userModel->findUserById( $_SESSION["userid"] ),
-						"gallery" => $this->galleryModel->getAllEditedImages(),
-						"countUnreadNotifs" => $this->notificationsModel->getCountUnreadNotifications( $_SESSION['userid'] )
+						"userData" => $this->user_model->findUserById( $_SESSION["userid"] ),
+						"gallery" => $this->gallery_model->getAllEditedImages(),
+						"countUnreadNotifs" => $this->notifications_model->getCountUnreadNotifications( $_SESSION['userid'] )
 					];
 					if ( isset( $data[0] ) && isset( $data[1] ) && ( isset( $data[0] ) && $data[0] === "notificationsemail" ) && ( isset( $data[1] ) && ( $data[1] === "1" || $data[1] === "0" ) ) ) {
 						switch( $_SERVER["REQUEST_METHOD"] ) {
@@ -172,11 +162,11 @@
 								$this->viewData["success"] = "true";
 							break;
 							case "POST":
-								if ( ( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) && $this->userMiddleware->validateUserToken( $_POST["token"] ) && ( isset( $_POST["btn-change-preference"] ) && !empty( $_POST["btn-change-preference"] ) ) ) {
+								if ( ( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) && $this->user_middleware->validateUserToken( $_POST["token"] ) && ( isset( $_POST["btn-change-preference"] ) && !empty( $_POST["btn-change-preference"] ) ) ) {
 									unset( $_POST["btn-change-preference"] );
-									if ( $this->userModel->change_preference_email_notifs( $_SESSION["userid"], $data[1] ) ) {
+									if ( $this->user_model->change_preference_email_notifs( $_SESSION["userid"], $data[1] ) ) {
 										$this->viewData["success"] = "true";
-										$this->viewData["data"]["userData"] = $this->userModel->findUserById( $_SESSION["userid"] );
+										$this->viewData["data"]["userData"] = $this->user_model->findUserById( $_SESSION["userid"] );
 									} else {
 										$this->viewData["success"] = "false";
 										$this->viewData["msg"] = "Failed to change your notifications preference !";
@@ -225,14 +215,14 @@
 		public function 				editing ()
 		{
 			try {
-				if ( !$this->userMiddleware->isSignin( $_SESSION ) ) {
+				if ( !$this->user_middleware->isSignin( $_SESSION ) ) {
 					header("Location: /signin");			
 				} else {
 					$this->viewData["data"] = [
-						"gallery" => $this->galleryModel->getAllEditedImages(),
-						"userData" => $this->userModel->findUserById( $_SESSION["userid"] ),
-						"userGallery" => $this->galleryModel->userGallery( $_SESSION["username"] ),
-						"countUnreadNotifs" => $this->notificationsModel->getCountUnreadNotifications( $_SESSION['userid'] )
+						"gallery" => $this->gallery_model->getAllEditedImages(),
+						"userData" => $this->user_model->findUserById( $_SESSION["userid"] ),
+						"userGallery" => $this->gallery_model->userGallery( $_SESSION["username"] ),
+						"countUnreadNotifs" => $this->notifications_model->getCountUnreadNotifications( $_SESSION['userid'] )
 					];
 					switch( $_SERVER["REQUEST_METHOD"] ) {
 						case "GET":
@@ -241,10 +231,10 @@
 						case "POST":
 							if (
 								( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) &&
-								$this->userMiddleware->validateUserToken( $_POST["token"] ) &&
+								$this->user_middleware->validateUserToken( $_POST["token"] ) &&
 								( isset( $_POST["btn-save"] ) && !empty( $_POST["btn-save"] ) ) 
 							) {
-								if ( $error = $this->userMiddleware->validateDescription( $_POST["description"] ) ) {
+								if ( $error = $this->user_middleware->validateDescription( $_POST["description"] ) ) {
 									$this->viewData["success"] = "false";
 									$this->viewData["msg"] = $error;
 								} else {
@@ -259,16 +249,16 @@
 									if ( $srcFinaleImg = $this->makeMixedImage( $this->viewData["data"]["userData"], $destPath, $srcPath, intval($_POST["x"]), intval($_POST["y"]) ) ) {
 										if ( file_exists( $pathFile ) ) { unlink( $pathFile ); }
 										$pathFinaleImg = self::transformPathFileToUrl( $srcFinaleImg );
-										if ( $this->galleryModel->addImage([ 'id' => $_SESSION['userid'], 'src' => $pathFinaleImg, 'description' => $_POST['description'] ]) ) {
+										if ( $this->gallery_model->addImage([ 'id' => $_SESSION['userid'], 'src' => $pathFinaleImg, 'description' => $_POST['description'] ]) ) {
 											$this->viewData["success"] = "true";
 											$this->viewData["msg"] = "Image has been saved successfully !";
-											$this->viewData["data"]["gallery"] = $this->galleryModel->getAllEditedImages();
-											$this->viewData["data"]["userGallery"] = $this->galleryModel->userGallery( $_SESSION["username"] );
+											$this->viewData["data"]["gallery"] = $this->gallery_model->getAllEditedImages();
+											$this->viewData["data"]["userGallery"] = $this->gallery_model->userGallery( $_SESSION["username"] );
 										} else {
 											$this->viewData["success"] = "false";
 											$this->viewData["msg"] = "Failed to create final image !";
-											$this->viewData["data"]["gallery"] = $this->galleryModel->getAllEditedImages();
-											$this->viewData["data"]["userGallery"] = $this->galleryModel->userGallery( $_SESSION["username"] );
+											$this->viewData["data"]["gallery"] = $this->gallery_model->getAllEditedImages();
+											$this->viewData["data"]["userGallery"] = $this->gallery_model->userGallery( $_SESSION["username"] );
 										}
 									} else {
 										if ( file_exists( $pathFile ) ) { unlink( $pathFile ); }
@@ -291,7 +281,7 @@
 		public function					logout()
 		{
 			try {
-				if ( !$this->userMiddleware->isSignin( $_SESSION ) ) {
+				if ( !$this->user_middleware->isSignin( $_SESSION ) ) {
 					header("Location: /signin");			
 				} else {
 					session_destroy();
