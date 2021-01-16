ab3d16f5ad92d1e002ecae5f8b36ccdc48efccfb
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2021-01-16 17:36:46 +0100

    rename class router

--- index.php
+++ index.php
@@ -17,5 +17,5 @@
 	$modules = [ROOT, APP, CONTROLLERS, MODELS, VIEWS, HELPERS, MIDDLEWARES, CONFIG, PUBLIC_DIR, IMAGES];
 	set_include_path( get_include_path() . PATH_SEPARATOR . implode(PATH_SEPARATOR, $modules) );
 	spl_autoload_register('spl_autoload', false);
-	new Router();
+	new router();
 ?>
