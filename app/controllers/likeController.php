4108d9520ff8eb6fe0a6312a396a33e59754784d
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2021-01-18 20:17:37 +0100

    Filter POST variables while like or unlike an image

--- app/controllers/likeController.php
+++ app/controllers/likeController.php
@@ -14,45 +14,38 @@
 		
 		public function 				add ()
 		{
-			if ( !$this->user_middleware->isSignin( $_SESSION ) ) {
-				$this->viewData = [
-					"success"=> "false",
-					"msg" => "You need to login first !"
-				];
-			} else if ( ( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) && $this->user_middleware->validateUserToken( $_POST["token"] ) ) {
-				try {
-					if ( $this->gallery_middleware->isImageExist( $_POST["id"] ) ) {
-						if ( !$this->like_model->isLikeExists( $_POST["id"], $_SESSION['userid'] ) ) {
-							if ( !$this->like_model->likeImage( $_POST["id"], $_SESSION['userid'] ) ) {
-								$this->viewData = [
-									"success" => "false",
-									"msg" => "Failed to like the image !"
-								];
+			try {
+				if ( !$this->user_middleware->isSignin( $_SESSION ) ) {
+					$this->viewData = [ "success"=> "false", "msg" => "You need to login first !" ];
+				} else if ( $this->helper->isRequestPOST( $_SERVER["REQUEST_METHOD"] ) ) {
+					if ( $_POST = $this->helper->filter_inputs( "POST", array(
+							'token' => FILTER_SANITIZE_STRING,
+							'id' => FILTER_SANITIZE_STRING
+						))
+					) {
+						if ( ( isset( $_POST["token"] ) && !empty( $_POST["token"] ) ) && $this->user_middleware->validateUserToken( $_POST["token"] ) ) {
+							if ( !$this->gallery_middleware->isImageExist( $_POST["id"] ) ) {
+								$this->viewData = [ "success" => "false", "msg" => "The image is not found !" ];
+							} else if ( $this->like_model->isLikeExists( $_POST["id"], $_SESSION['userid'] ) ) {
+								if ( $this->like_model->unlikeImage( $_POST["id"], $_SESSION['userid'] ) ) {
+									$this->viewData = [ "success" => "true" ];
+								} else {
+									$this->viewData = [ "success" => "false", "msg" => "Failed to unlike the image !" ];
+								}
 							} else {
-								$this->viewData = [ "success" => "true" ];
-							}
-						} else {
-							if ( !$this->like_model->unlikeImage( $_POST["id"], $_SESSION['userid'] ) ) {
-								$this->viewData = [
-									"success" => "false",
-									"msg" => "Failed to unlike the image !"
-								];
-							} else {
-								$this->viewData = [ "success" => "true" ];
+								if ( $this->like_model->likeImage( $_POST["id"], $_SESSION['userid'] ) ) {
+									$this->viewData = [ "success" => "true" ];
+								} else {
+									$this->viewData = [ "success" => "false", "msg" => "Failed to like the image !" ];
+								}
 							}
 						}
 					} else {
-						$this->viewData = [
-							"success" => "false",
-							"msg" => "The image is not found !"
-						];
+						$this->viewData = [ "success" => "false", "msg" => "Couldn't like or unlike the image, try later !" ];
 					}
-				} catch ( Exception $e ) {
-					$this->viewData = [
-						"success" => "false",
-						"msg" => "Something goes wrong while like the image !"
-					];
 				}
+			} catch ( Exception $e ) {
+				$this->viewData = [ "success" => "false", "msg" => "Something goes wrong while like or un like the image, try later !" ];
 			}
 			die( json_encode($this->viewData, JSON_FORCE_OBJECT) );
 		}
@@ -60,22 +53,19 @@
 		public function					userswholikes ()
 		{
 			try {
-				if ( !$this->gallery_middleware->isImageExist( $_GET["id"] ) ) {
-					$this->viewData = [
-						"success" => "false",
-						"msg" => "The image is not found !"
-					];
-				} else {
-					$this->viewData = [
-						"success" => "true",
-						"users" => $this->like_model->getUsersLikeImage( $_GET["id"] )
-					];
+				if ( $this->helper->isRequestGET( $_SERVER["REQUEST_METHOD"] ) ) {
+					if ( $_GET = $this->helper->filter_inputs( "GET", array( 'id' => FILTER_SANITIZE_NUMBER_INT )) ) {
+						if ( !$this->gallery_middleware->isImageExist( $_GET["id"] ) ) {
+							$this->viewData = [ "success" => "false", "msg" => "The image is not found !" ];
+						} else {
+							$this->viewData = [ "success" => "true", "users" => $this->like_model->getUsersLikeImage( $_GET["id"] ) ];
+						}
+					} else {
+						$this->viewData = [ "success" => "false", "msg" => "Couldn't load users who like the specified image, try later !" ];
+					}
 				}
 			} catch ( Exception $e ) {
-				$this->viewData = [
-					"success" => "false",
-					"msg" => "Something goes wrong while get users who liked the image !"
-				];
+				$this->viewData = [ "success" => "false", "msg" => "Something goes wrong while get users who liked the image, try later !" ];
 			}
 			die( json_encode($this->viewData) );
 		}
