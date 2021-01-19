eddfe1045380d06822524475a8de7b6ac77dfad1
GUISMI Ayoub <46103801+guismiAy@users.noreply.github.com>
2021-01-19 11:52:50 +0100

    Add a middleware for validate coordinates of the sticker

--- app/middlewares/GalleryMiddleware.php
+++ app/middlewares/GalleryMiddleware.php
@@ -5,6 +5,13 @@
 	class GalleryMiddleware extends Middleware
 	{
 
+		public function					validateCoordinatesSticker ( $x, $y )
+		{
+			if ( ( isset( $_POST["x"] ) && intval( $_POST["x"] ) < 0 ) || ( isset( $_POST["y"] ) && intval( $_POST["y"] ) < 0 ) ) {
+				return "Coordinates of the sticker must be positive";
+			}
+		}
+
 		public function					validateDescription ( $description )
 		{
 			return $this->validateImageDescription( $description );
