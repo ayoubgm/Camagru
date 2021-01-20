dcbf8b1b14233b69dfdfb8ae7f42d6f266b2c753
ayoub guismi <i.guismi@gmail.com>
2021-01-20 19:14:09 +0100

    Fix a regex exp of username

--- public/js/home/signup.js
+++ public/js/home/signup.js
@@ -45,7 +45,8 @@ const 			validateLastName = ( lastname ) => {
 }
 // Validate lastname by run some regex
 const 			validateUsername = ( username ) => {
-    if ( !/^(?=.{3,20}$)(?![-_.])(?!.*[-_.]{2})[a-zA-Z0-9._-]+(?<![-_.])$/.test( username.value ) ) {
+    if (username.value.length < 3 || username.value.length > 20 || !/^[a-zA-Z]+(([-_.]?[a-zA-Z0-9])?)+$/.test(username.value)){
+    // if ( !/^(?=.{3,20}$)(?![-_.])(?!.*[-_.]{2})[a-zA-Z0-9._-]+(?<![-_.])$/.test( username.value ) ) {
         setError(username, "The username should contain between 3 and 20 letters or numbers ( -, _ or . ) !");
         return false;
     } else {
